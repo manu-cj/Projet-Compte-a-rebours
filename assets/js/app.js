815ab2898bdc5f67e80ca798795f1ed9d3a8b4a0
manucarenjot <carenjotje@gmail.com>
2021-12-12 15:28:05 +0100

    Compte à rebours

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,5 @@
-let Hours = document.getElementById('heures');
-let minuts = document.getElementById('minutes');
+let hours = document.getElementById('heures');
+let minutes = document.getElementById('minutes');
 let seconds = document.getElementById('secondes');
 
 let startButton = document.getElementById('start');
@@ -7,17 +7,39 @@ let pauseButton = document.getElementById('pause');
 let resetButton = document.getElementById('reset');
 
 let compteARebours = document.getElementById('compteARebours');
+let compteur = 0;
 
 function start() {
 
+    time = setInterval(() => {
+        if (userSeconds >= 0){
+            compteARebours.innerHTML = '00 : ' + userMinutes + ' : ' + userSeconds;
+        }
+        else if ( userMinutes >= 0) {
+            compteARebours.innerHTML = '00 : ' + userMinutes + userSeconds;
+        }
+        userSeconds--
+        if (userSeconds === 0) {
+            userMinutes--
+            userSeconds = 60;
+        }
+        }, 1000)
 }
 
+let userHours;
+let userMinutes;
+let userSeconds;
 
-
+startButton.addEventListener("click", () => {
+    userHours = parseInt(document.querySelector("#heures").value);
+    userMinutes = parseInt(document.querySelector("#minutes").value);
+    userSeconds = parseInt(document.querySelector("#secondes").value);
+    console.log(userHours);
+})
 
 
 startButton.addEventListener("click", () => {
-
+    start()
 })
 
 pauseButton.addEventListener("click", () => {
