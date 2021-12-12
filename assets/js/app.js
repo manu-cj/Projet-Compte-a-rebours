0232e923be877ec57ad300334dc92d5fc3f53d77
manucarenjot <carenjotje@gmail.com>
2021-12-12 14:25:16 +0100

    Compte à rebours

---
+++ assets/js/app.js
@@ -0,0 +1,29 @@
+let Hours = document.getElementById('heures');
+let minuts = document.getElementById('minutes');
+let seconds = document.getElementById('secondes');
+
+let startButton = document.getElementById('start');
+let pauseButton = document.getElementById('pause');
+let resetButton = document.getElementById('reset');
+
+let compteARebours = document.getElementById('compteARebours');
+
+function start() {
+
+}
+
+
+
+
+
+startButton.addEventListener("click", () => {
+
+})
+
+pauseButton.addEventListener("click", () => {
+
+})
+
+resetButton.addEventListener("click", () => {
+
+})
