aadf4edd12d2b745f94388d3505f53a65bc9777c
manucarenjot <carenjotje@gmail.com>
2021-12-12 20:26:38 +0100

    Compte à rebours

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,78 +9,90 @@ let resetButton = document.getElementById('reset');
 
 let compteARebours = document.getElementById('compteARebours');
 
-function start() {
+let Time = function (start, pause, reset, secondes, minutes, hours) {
+    function start() {
 
-    time = setInterval(() => {
-        if (compteurSeconds >= -1 && compteurMinutes >= -1 && compteurHours >= -1 && compteurDays >= 0){
-            compteARebours.innerHTML = compteurDays +' jours 0' + compteurHours + ' heures 0' + compteurMinutes + ' minutes 0' + compteurSeconds;
-        }
-       if (compteurSeconds >= 61) {
-           compteurSeconds -=61;
-            compteurMinutes+=1
-        }
-        if (compteurMinutes >= 61) {
-            compteurMinutes -=61;
-            compteurHours+=1
-        }
-        if (compteurHours >= 25) {
-            compteurHours -=25;
-            compteurDays+=1
-        }
+        time = setInterval(() => {
+            if (compteurSeconds >= -1 && compteurMinutes >= -1 && compteurHours >= -1 && compteurDays >= 0) {
+                compteARebours.innerHTML = compteurDays + ' jours 0' + compteurHours + ' heures 0' + compteurMinutes + ' minutes 0' + compteurSeconds;
+            }
+            if (compteurSeconds >= 61) {
+                compteurSeconds -= 61;
+                compteurMinutes += 1
+            }
+            if (compteurMinutes >= 61) {
+                compteurMinutes -= 61;
+                compteurHours += 1
+            }
+            if (compteurHours >= 25) {
+                compteurHours -= 25;
+                compteurDays += 1
+            }
 
-        compteurSeconds--;
+            compteurSeconds--;
 
-        if (compteurSeconds === -1) {
-            compteurMinutes--
-            compteurSeconds = 60;
-        }
-        if (compteurMinutes === -1) {
-            compteurHours--
-            compteurMinutes = 60;
-        }
-        if (compteurHours === -1) {
-            compteurDays--
-            compteurHours = 60;
-        }
+            if (compteurSeconds === -1) {
+                compteurMinutes--
+                compteurSeconds = 60;
+            }
+            if (compteurMinutes === -1) {
+                compteurHours--
+                compteurMinutes = 60;
+            }
+            if (compteurHours === -1) {
+                compteurDays--
+                compteurHours = 60;
+            }
 
         }, 1000)
-    days.value = "";
-    hours.value = "";
-    minutes.value = "";
-    seconds.value = "";
+
+
+    }
+
+
+    function pause() {
+        clearInterval(time);
+    }
+
+
+
+    startButton.addEventListener("click", () => {
+        compteurDays = parseInt(document.getElementById('jours').value);
+        compteurHours = parseInt(document.getElementById('heures').value);
+        compteurMinutes = parseInt(document.getElementById('minutes').value);
+        compteurSeconds = parseInt(document.getElementById('secondes').value);
+        console.log(compteurHours);
+    })
+
+
+    startButton.addEventListener("click", () => {
+        start();
+    })
+
+    pauseButton.addEventListener("click", () => {
+        pause()
+    })
+
+    resetButton.addEventListener("click", () => {
+        compteurHours = 0;
+        compteurMinutes = 0;
+        compteurSeconds = 0;
+    })
 
 }
 
-function pause() {
-    clearInterval(time);
-}
-
+let compteurDays
 let compteurHours;
 let compteurMinutes;
 let compteurSeconds;
 
-startButton.addEventListener("click", () => {
-    compteurDays = parseInt(document.getElementById('jours').value);
-    compteurHours = parseInt(document.getElementById('heures').value);
-    compteurMinutes = parseInt(document.getElementById('minutes').value);
-    compteurSeconds = parseInt(document.getElementById('secondes').value);
-    console.log(compteurHours);
-})
-
+let createTimer = new Time(startButton, pauseButton, resetButton, compteurSeconds, compteurMinutes, compteurHours, compteurDays)
+createTimer
 
 startButton.addEventListener("click", () => {
-            start();
+    days.value = "";
+    hours.value = "";
+    minutes.value = "";
+    seconds.value = "";
 })
-
-pauseButton.addEventListener("click", () => {
-    pause()
-})
-
-resetButton.addEventListener("click", () => {
- compteurHours = 0;
- compteurMinutes = 0;
- compteurSeconds = 0;
-})
-
-
 
